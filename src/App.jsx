fab3d2e27af916dea3f035ba519b5554e51d1369
Nicolopez93 <nicolopezarq@gmail.com>
2023-11-24 10:11:19 -0300

    primer commit de la entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 
+import { Route } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import { ContextProvider } from "./Components/utils/global.context";
 
 
 function App() {
   return (
+      <ContextProvider>
       <div className="App">
           <Navbar/>
           <Footer/>
       </div>
+      </ContextProvider>
   );
 }
 
--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,15 +1,43 @@
-import { createContext } from "react";
+import React, { createContext, useReducer, useContext, useMemo, useEffect } from 'react';
 
-export const initialState = {theme: "", data: []}
+export const initialState = { theme: "light", data: [] };
 
-export const ContextGlobal = createContext(undefined);
+const ContextGlobal = createContext(undefined);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'TOGGLE_THEME':
+      return { ...state, theme: state.theme === 'light' ? 'dark' : 'light' };
+    case 'SET_API_DATA':
+      return { ...state, data: action.payload };
+    default:
+      return state;
+  }
+};
 
 export const ContextProvider = ({ children }) => {
-  //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+        const data = await response.json();
+        dispatch({ type: 'SET_API_DATA', payload: data });
+      } catch (error) {
+        console.error('Error fetching API data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const contextValue = useMemo(() => ({ state, dispatch }), [state, dispatch]);
 
   return (
-    <ContextGlobal.Provider value={{}}>
+    <ContextGlobal.Provider value={contextValue}>
       {children}
     </ContextGlobal.Provider>
   );
 };
+
+export const useContextGlobal = () => useContext(ContextGlobal);
+
