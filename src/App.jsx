caa4eb15f8370a3ee9ae213c247964a3723d71c3
nicolopez93 <nicolopezarq@gmail.com>
2023-11-25 14:38:33 -0300

    se realizo correctamente el PAHT de las ROUTAS

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 
-import { Route } from "react-router-dom";
+import React from 'react';
+import {BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from './Routes/Home'
+import Favs from './Routes/Favs'
+import Contact from './Routes/Contact'
+import Detail from './Routes/Detail'
 import { ContextProvider } from "./Components/utils/global.context";
+import './App.css'
 
 
 function App() {
   return (
       <ContextProvider>
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+        <Router>
+          <React.Fragment>
+            <Navbar/>
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/home" element={<Home/>} />
+              <Route path="/favs" element={<Favs/>} />
+              <Route path="/contact" element={<Contact/>} />
+              <Route path="/detail" element={<Detail/>} />
+              <Route path='' element={()=> <h1>NO FUNCIONA HERMANO</h1>} />
+            </Routes>
+            <Footer/>
+          </React.Fragment>
+        </Router> 
       </ContextProvider>
-  );
+  )
 }
 
 export default App;
