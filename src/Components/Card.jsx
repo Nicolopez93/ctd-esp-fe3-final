411a603236e5e6bc7fac65e4355d2cc2be7be7cb
nicolopez93 <nicolopezarq@gmail.com>
2023-11-27 09:49:12 -0300

    fallo en el intento de card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,18 +1,36 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 
 const Card = ({ name, username, id }) => {
 
   const addFav = ()=>{
-    // Aqui iria la logica para agregar la Card en el localStorage
+  
+    // Obtener las tarjetas existentes del localStorage
+    const existingCards = JSON.parse(localStorage.getItem('favs')) || [];
+  
+    // Verificar si la tarjeta actual ya está en favoritos
+    const isCardInFavs = existingCards.some(card => card.id === id);
+
+    if (!isCardInFavs) {
+      const updatedCards = [...existingCards, { id, name, username }];
+      localStorage.setItem('favs', JSON.stringify(updatedCards));
+      alert('Se agrego la card correctamente a favoritos');
+    } else {
+      alert('La card ya esta en favoritos');
+    }
   }
 
   return (
     <div className="card">
         {/* En cada card deberan mostrar en name - username y el id */}
-
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
+        <Link to={`/dentist/${id}`}>
+          <p>Name: {name}</p>
+          <p>Username: {username}</p>
+          <p>ID: {id}</p>
+        </Link>
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
         <button onClick={addFav} className="favButton">Add fav</button>
     </div>
