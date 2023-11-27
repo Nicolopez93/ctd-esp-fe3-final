ac82728ee3a461eaa1a71f75d362f071527f8aa0
Nicolopez93 <nicolopezarq@gmail.com>
2023-11-27 14:30:07 -0300

    se realizo el footer con sus estilos css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import {BrowserRouter as Router,Routes, Route } from "react-router-dom";
-import Footer from "./Components/Footer";
+import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from './Routes/Home'
 import Favs from './Routes/Favs'
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './footer.css';
+import Facebook from '../../asserts/ico-facebook.png';
+import Instagram from '../../asserts/ico-instagram.png';
+import WhatsApp from '../../asserts/ico-whatsapp.png';
+import Tiktok from '../../asserts/ico-tiktok.png';
+import DH from '../../asserts/DH.png';
+
+const Footer = () => {
+  return (
+    <footer className="Footer">
+    <section className="DigitalHouse">
+      <div className="RedesSociales">
+        <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+          <img src={Facebook} alt='facebook-logo' />
+        </a>
+        <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+          <img src={Instagram} alt='instagram-logo' />
+        </a>
+        <a href="https://www.whatsapp.com" target="_blank" rel="noreferrer">
+          <img src={WhatsApp} alt='whatsapp-logo' />
+        </a>
+        <a href="https://www.tiktok.com" target="_blank" rel="noreferrer">
+          <img src={Tiktok} alt='tiktok-logo' />
+        </a>
+      </div>
+
+      <div className="Logo">
+        <p>Powered by</p>
+        <img src={DH} alt='DH' />
+      </div>
+    </section>
+</footer>
+
+  )
+}
+
+export default Footer
